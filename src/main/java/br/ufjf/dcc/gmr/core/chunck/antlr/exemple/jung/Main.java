e155c8ada0b017a6d835d12212d64df0430894d7
LucasGoldner <lucas.goldner@ice.ufjf.br>
2020-10-20 13:39:29 -0300

    jung mas sem jung

---
+++ src/main/java/br/ufjf/dcc/gmr/core/chunck/antlr/exemple/jung/Main.java
@@ -0,0 +1,18 @@
+package br.ufjf.dcc.gmr.core.chunck.antlr.exemple.jung;
+
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.event.GraphEvent;
+import edu.uci.ics.jung.graph.event.GraphEvent.Vertex;
+
+
+public class Main {
+    public static void main(String[] args) {
+//        Graph g = new DirectedSparseGraph();
+//        
+//        Vertex v1 = new DirectedSparseVertex();
+//	Vertex v2 = new DirectedSparseVertex();
+//        
+//        DirectedEdge e = new DirectedSparseEdge(v1, v2);
+    }
+}
